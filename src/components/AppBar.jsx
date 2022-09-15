18f5b44e108fe91362aa1d85eb78fc76af606c6c
Dmytro Samyliak <dsamyliak@gmail.com>
2022-09-15 23:44:59 +0300

    update AppBar.jsx code

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -6,25 +6,33 @@ import { useSelector } from 'react-redux';
 export const AppBar = () => {
   const isLoggedIn = useSelector(state => state.user.isLoggedIn);
 
-    return (
-        <header
-            style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                padding: 10,
-                borderBottom: '1px solid black',
-                marginBottom: 12,
-            }}
-        >
-            <nav>
-                <Link to="/" style={{ paddingRight: 10 }}>
-                    Main
-                </Link>
-                {!isLoggedIn && <Link to="/login" style={{ paddingRight: 10 }}>Log In</Link>}
-                {isLoggedIn && <Link to="/dashboard" style={{ paddingRight: 10 }}>Dashboard</Link>}
-                {/* <Link to="/" onClick={() => dispatch(logOut())}>          Log Out        </Link> */}
-            </nav>
-            {isLoggedIn && <UserMenu />}
-        </header>
-    );
+  return (
+    <header
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: 10,
+        borderBottom: '1px solid black',
+        marginBottom: 12,
+      }}
+    >
+      <nav>
+        <Link to="/" style={{ paddingRight: 10 }}>
+          Main
+        </Link>
+        {!isLoggedIn && (
+          <Link to="/login" style={{ paddingRight: 10 }}>
+            Log In
+          </Link>
+        )}
+        {isLoggedIn && (
+          <Link to="/dashboard" style={{ paddingRight: 10 }}>
+            Dashboard
+          </Link>
+        )}
+        {/* <Link to="/" onClick={() => dispatch(logOut())}>Log Out</Link> */}
+      </nav>
+      {isLoggedIn && <UserMenu />}
+    </header>
+  );
 };
